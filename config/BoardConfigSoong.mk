77cc8b21d9c09cae33d369ce616c562025df56bf
sackclothxashes <george140201@gmail.com>
2021-05-20 18:22:59 +0530

    soong: introduce TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY (#7)
    Set TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY to the key code to skip in InputDispatcher
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I682913c29bdb92409b05aea7dfa1bcfa3a74653f
    
    Co-authored-by: daniml3 <danimoral1001@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -31,6 +31,7 @@ SOONG_CONFIG_protonGlobalVars += \
     additional_gralloc_10_usage_bits \
     supports_extended_compress_format \
     target_init_vendor_lib \
+    target_inputdispatcher_skip_event_key \
     target_ld_shim_libs \
     uses_qti_camera_device
 
@@ -51,10 +52,12 @@ SOONG_CONFIG_protonQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PR
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
+TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY ?= 0
 
 # Soong value variables
 SOONG_CONFIG_protonGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_protonGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+SOONG_CONFIG_protonGlobalVars_target_inputdispatcher_skip_event_key := $(TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY)
 SOONG_CONFIG_protonGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_protonGlobalVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
