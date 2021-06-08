caaad656d21263eabc79bedeb0f0efe0f047f7b3
Danny Lin <danny@kdrag0n.dev>
2021-06-08 09:17:17 -0700

    fonts: Add fallback variant of Roboto
    Inter is missing some glyphs that users have come to expect on Android,
    so use Roboto as a fallback to fix excessive tofu in some text.
    
    Because Inter's name has been replaced with "Roboto" for compatibility,
    the fallback variants are named "Roboto Fallback".
    
    Example of text with missing glyphs in Inter: ⅙⅐⅛⅑⅒¹½⅓¼⅕
    
    NB: requires frameworks/base commit "fonts: Use fallback variant of Roboto"

--- config/fonts.mk
+++ config/fonts.mk
@@ -4,3 +4,18 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
+PRODUCT_PACKAGES += \
+    RobotoFallback-BlackItalic.ttf \
+    RobotoFallback-Black.ttf \
+    RobotoFallback-BoldItalic.ttf \
+    RobotoFallback-Bold.ttf \
+    RobotoFallback-Italic.ttf \
+    RobotoFallback-LightItalic.ttf \
+    RobotoFallback-Light.ttf \
+    RobotoFallback-MediumItalic.ttf \
+    RobotoFallback-Medium.ttf \
+    RobotoFallback-Regular.ttf \
+    RobotoFallback-ThinItalic.ttf \
+    RobotoFallback-Thin.ttf 
+
