9c7915b2c06b880c1e87cff5af2f693365b3f6a4
Danny Lin <danny@kdrag0n.dev>
2021-06-08 18:04:02 -0700

    fonts: Switch to variable font for Roboto fallback
    This reduces Roboto Fallback's size from 3.7 MiB to 2.3 MiB.
    
    NB: requires frameworks/base commit "fonts: Switch to variable font for Roboto fallback"

--- config/fonts.mk
+++ config/fonts.mk
@@ -6,16 +6,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
 PRODUCT_PACKAGES += \
-    RobotoFallback-BlackItalic.ttf \
-    RobotoFallback-Black.ttf \
-    RobotoFallback-BoldItalic.ttf \
-    RobotoFallback-Bold.ttf \
-    RobotoFallback-Italic.ttf \
-    RobotoFallback-LightItalic.ttf \
-    RobotoFallback-Light.ttf \
-    RobotoFallback-MediumItalic.ttf \
-    RobotoFallback-Medium.ttf \
-    RobotoFallback-Regular.ttf \
-    RobotoFallback-ThinItalic.ttf \
-    RobotoFallback-Thin.ttf 
+    RobotoFallback-VF.ttf 
 
