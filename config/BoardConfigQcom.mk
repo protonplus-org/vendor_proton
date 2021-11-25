b732ffa02d5ad45a8d7bc950a876942ef4ae1978
palaych <palaychm@yandex.ru>
2021-11-26 02:08:25 +0900

    Add soong namespaces for qcom display commonsys
    Change-Id: I0840fae0306480bd2407a6193e10cb9977442cf3
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -108,9 +108,10 @@ ifneq ($(USE_DEVICE_SPECIFIC_DATASERVICES),true)
     PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/dataservices
 endif
 
-# Add display-commonsys-intf to PRODUCT_SOONG_NAMESPACES for QSSI supported platforms
+# Add display-commonsys and display-commonsys-intf to PRODUCT_SOONG_NAMESPACES for QSSI supported platforms
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/commonsys/display \
     vendor/qcom/opensource/commonsys-intf/display \
     vendor/qcom/opensource/display
 endif
