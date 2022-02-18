4e70c4bbfb12a8ad8cd74e21b8272e6cbdb0cd60
Pig <pig.priv@gmail.com>
2022-02-18 16:42:38 +0000

    soong: add target_shim_libs

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -31,6 +31,7 @@ SOONG_CONFIG_protonGlobalVars += \
     additional_gralloc_10_usage_bits \
     supports_extended_compress_format \
     target_init_vendor_lib \
+    target_ld_shim_libs \
     uses_qti_camera_device
 
 SOONG_CONFIG_NAMESPACES += protonQcomVars
@@ -54,6 +55,7 @@ TARGET_INIT_VENDOR_LIB ?= vendor_init
 # Soong value variables
 SOONG_CONFIG_protonGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_protonGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+SOONG_CONFIG_protonGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_protonGlobalVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
 else
