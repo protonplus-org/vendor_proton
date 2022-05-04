b1c0993a12333e525f731f53b4cf274c660958e4
Md Abu Talha <rokusenpai06@gmail.com>
2022-05-04 17:53:43 +0000

    soong: checkout existing soong from los

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -30,17 +30,17 @@ SOONG_CONFIG_NAMESPACES += protonGlobalVars
 SOONG_CONFIG_protonGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
-    supports_extended_compress_format \
     target_init_vendor_lib \
     target_inputdispatcher_skip_event_key \
     target_ld_shim_libs \
-    target_surfaceflinger_udfps_lib \
-    uses_qti_camera_device
+    target_surfaceflinger_udfps_lib 
 
 SOONG_CONFIG_NAMESPACES += protonQcomVars
 SOONG_CONFIG_protonQcomVars += \
     legacy_hw_disk_encryption \
-    uses_pre_uplink_features_netmgrd
+    supports_extended_compress_format \
+    uses_pre_uplink_features_netmgrd \
+    uses_qti_camera_device
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
@@ -49,10 +49,10 @@ SOONG_CONFIG_protonGlobalVars += \
 endif
 
 # Soong bool variables
-SOONG_CONFIG_protonGlobalVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
-SOONG_CONFIG_protonGlobalVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 SOONG_CONFIG_protonQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
+SOONG_CONFIG_protonQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_protonQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_protonQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
