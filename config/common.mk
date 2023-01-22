e86b4da1c9a51d49ebaa18c6828dc60694b20064
prajwal-walia <prajwalahluwalia5@gmail.com>
2023-01-22 12:12:14 +0000

    config: Build Aperture camera app

--- config/common.mk
+++ config/common.mk
@@ -201,6 +201,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Apps
+PRODUCT_PACKAGES += \
+    Aperture
+
 # Fonts
 include vendor/proton/config/fonts.mk
 
