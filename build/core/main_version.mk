092fc3b8baff28684a9a6255e76a762641b4bb46
baka-hokage <ayushbehera007@gmail.com>
2023-05-20 15:10:32 +0530

    vendor: Make GApps optional

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -11,6 +11,14 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.proton.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
     ro.modversion=$(PROTON_VERSION) 
 
+ifeq ($(WITH_GAPPS),true)
+ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.proton.ziptype=gapps
+else
+ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.proton.ziptype=vanilla
+endif
+
 # ProtonPlus Platform Display Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.proton.display.version=$(PROTON_VERSION)
